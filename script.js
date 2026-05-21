9d396429f0883963b5f42050f5a19112d9d8f838
henrico0souza <henrico.souzacr7@gmail.com>
2026-05-21 12:07:48 -0300

    continução do site

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
 function executarSistema() {
-
-    //dados de entrada
-    const nome = document.getElementById("inputNome").Value;
-    const idade = parseInt(document.getElementById("inputIdade").Value);
-    const valor = parseFloat(document.getElementById("inputValor").Value);
-    const cupom = document.getElementById("inputCupom").Value === "true";
+    const nome = document.getElementById("inputNome").value;
+    const idade = parseInt(document.getElementById("inputIdade").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true";
 
     //dados de saida
     const msg = document.getElementById("mensagem-autorizacao");
     const lista = document.getElementById("lista-estoque");
     const relatorio = document.getElementById("relatorio-final");
 
-    //validação para campos de vazios
-    if (!nome || isNaN(idade) || isNaN(valor)) {
-        alert("Por favor, preencha todos os campos!");
+    // validação para campos vazios
+
+    if(!nome || isNaN(idade) || isNaN(valor)){
+        alert("Por favor, preencha os dados!");
         return;
-
     }
-
-    //regra de negocio
-    if (idade >= 16) {
+    
+    // regra de negócio
+    if (idade >= 16){
         msg.innerText = `Venda autorizada: ${nome}`;
-        msg.style.color = "#00ff88";
+        msg.style.color = "#1aff00";
 
         //desconto
-        let valorfinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+        let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
 
         //estoque
-        let estoque = ["Placa de video", "processdor", "Memoria RAM"];
-        lista.innerHTML = ""; //limpar a lista anterior
+        let estoque = ["placa de video", "processador", "memoria RAM"];
+        lista.innerHTML = ""; // limpa a lista anterior
+
+        estoque. forEach(Item => {
+            let li = document.createElement("li");
+        li.innerText = `Item) ${Item} reservado.`;
+        lista.appendChild(li);
+        });
+
+        //relatorio
+        relatorio.style.display = "block";
+        relatorio.innerHTML = `
+        <strong> RESUMO DO PEDIDO <\strong><br>
+        cliente: ${nome} <br>
+        Total Original: R$ ${valor.toFixed(2)} <br>
+        <strong> Total com Desconto: R$ ${valorFinal.toFixed(2)} <\strong>
+         `;
+
+    } else {
+        msg.innerText = 'Venda Bloqeada: Menor de 16 anos.';
+        msg.style.color = "#ff4444";
+        relatorio.style.display = "nome";
+        lista.innerHTML = "";
+
         
 
     }
 
-
-
-
-
-}
+    
+}   
